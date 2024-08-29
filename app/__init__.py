ff83db37871357da43067a83abe830211673ee6c
valeria <vcasadoh@gmail.com>
2024-08-29 23:52:27 +0200

    postgress first steps

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, g
+from flask_oidc import OpenIDConnect
 
 def before_request_function():
     print('Should not print in testing!')
@@ -9,16 +10,19 @@ def create_app():
     config = {
         "DEBUG": True,
         "SECRET_KEY": "secret",
-        'SUPPORTED_ISSUERS': ['https://accounts.google.com']
+        'SUPPORTED_ISSUERS': ['accounts.google.com']
 
     }
     app.config = app.config | config
     app.config["DEBUG"] = True
     app.config["SECRET_KEY"] = "secret"
+    app.config["OIDC_CLIENT_SECRETS"] = ''
 
     app.before_request(lambda: before_request_function())
 
     from .main import main 
     app.register_blueprint(main, url_prefix='/main')
+
+    # oidc = OpenIDConnect(app)
 
     return app
